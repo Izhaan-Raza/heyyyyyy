db22e25410060fb7e7f440dfdf4c448ac0211477
Izhaan Raza <66744194+Izhaan-Raza@users.noreply.github.com>
2024-01-10 23:56:43 +0530

    main

---
+++ public/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: [" Designer", " Developer", "Dreamer", "Student"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true,
+  });
+  
